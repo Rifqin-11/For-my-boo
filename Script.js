6422245515cbee188601bc81356618b77e28240f
Rifqi Naufal <rifqinaufal9009@gmail.com>
2024-02-14 01:45:38 +0700

    change distance in script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 var caunting = new Date("Oct 3, 2020 00:00:00").getTime();
 var x = setInterval(function (){
     var now = new Date().getTime();
-    var distance = caunting + now;
+    var distance = caunting - now;
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
